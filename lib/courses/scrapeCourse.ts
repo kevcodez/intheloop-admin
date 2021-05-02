8d7cc7b4f8830bdad126841648cc19f8202a24db
Kevin Grüneberg <k.grueneberg1994@gmail.com>
2021-05-02 14:04:34 +0200

    Scraping for egghead courses

--- lib/courses/scrapeCourse.ts
+++ lib/courses/scrapeCourse.ts
@@ -4,6 +4,8 @@ import ky from 'ky'
 export const scrapeCourse = async (url: string): Promise<Course | null> => {
   if (url.includes('udemy')) {
     return udemyCourse(url)
+  } else if (url.includes('egghead')) {
+    return eggheadCourse(url)
   }
 
   return null
@@ -17,8 +19,6 @@ const udemyCourse = async (url: string): Promise<Course | null> => {
   }
 
   const response = await ky.get(`http://localhost:3030/https://www.udemy.com/api-2.0/courses/${udemyId}?fields[course]=@all`).json<any>()
-
-  console.log(response)
 
   const courseInfo: CourseInfo = {
     authors: response.visible_instructors.map((it: any) => {
@@ -35,7 +35,35 @@ const udemyCourse = async (url: string): Promise<Course | null> => {
   }
 
   return {
-    id: udemyId,
+    id: 'udemy-' + udemyId,
     info: courseInfo,
+  }
+}
+
+const eggheadCourse = async (url: string): Promise<Course | null> => {
+  const response = await ky.get(`http://localhost:3030/${url}`).text()
+
+  const parser = new DOMParser()
+  const htmlDoc = parser.parseFromString(response, 'text/html')
+
+  const name = htmlDoc.querySelector('title')!!.text
+  const description = htmlDoc.querySelector('div.prose')!!.innerHTML
+  const author = htmlDoc.querySelector('span.font-semibold.text-base')!!.textContent!!
+  const image = htmlDoc.querySelector('a[href^="/lessons/"] noscript > img')?.getAttribute('src')!!
+  const createdAt = response.match(/"created_at":"(.*?)"/)?.[1]
+
+  const eggheadId = url.match(/courses\/(.+)/)?.[1]
+
+  return {
+    id: 'egghead-' + eggheadId!!,
+    info: {
+      description,
+      name,
+      url,
+      authors: [{ name: author }],
+      price: '$25', // some are free, some require membership which starts at $25/month
+      image,
+      publishedAt: createdAt,
+    },
   }
 }
